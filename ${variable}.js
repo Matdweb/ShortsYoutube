cda714cb539fca841e140594cfb6bd9c64737280
Matias Vargas <mat.dweb28[1>
2022-08-06 22:28:07 -0600

    ${variable} video

---
+++ ${variable}.js
@@ -0,0 +1,19 @@
+//Hola - hoy aprenderemos a como mostrar el valor de una varible entre comillas
+//Se debe usar esta sintaxis dentro de las comillas " %{variable} "
+
+let Apodo = "Mat_dweb"; 
+document.write(`Su apodo es <b>${Apodo}</b><br><br>`); 
+
+//De esta manera se muestra el valor de "Apodo"
+
+//tambien se puede aplicar con el prompt y con alert
+
+//prompt()
+let color = prompt(`Digite un color`); 
+document.write(`el color eligido es ${color}<br>`); 
+
+//alert
+let edad = prompt(`digite su edad`); 
+alert(`La edad del usuario es de ${edad}`)
+
+//Muchas gracias por ver!!
