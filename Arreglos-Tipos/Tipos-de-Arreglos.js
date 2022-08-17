991945f2c90e64784d60ebc3f002fa7df244b83b
Mat dweb <mat.dweb28@gmail.com>
2022-08-16 18:52:40 -0600

    Creacion de nuevo contenido: Arreglos-Tipos/

---
+++ Arreglos-Tipos/Tipos-de-Arreglos.js
@@ -0,0 +1,39 @@
+//Hola - en esta ocacion vamos a ver como son los
+//diferentes tipos de arreglos 
+
+//se pueden crear arreglos de diferentes elementos 
+// ya sean: 
+
+//  1.datos (informacion) 
+//  2. objetos
+//  3. arreglos
+
+//arreglo de datos 
+let varible = "varible"; 
+let Arreglo = ["texto", 124, variable]; 
+document.write(Arreglo); 
+
+//arreglo de objetos 
+let Objetos = [
+    {
+        Nickname: "Crack-281328",
+        Nivel: 100
+    },{
+        Nickname: "Gory30",
+        Nivel: 85
+    },{
+        Nickname: "turbo-fgl",
+        Nivel: 90
+    },{
+        Nickname: "The knight of gaming",
+        Nivel: 1000
+    }
+]; 
+console.log(Objetos); 
+
+//arreglo de arreglos
+let Componentes = [
+    Hardware = ["tarjeta madre", "CPU", "RAM", "ROM"],
+    Software = ["Sistemas operativos", "programas secundarios", "Aplicaciones"]
+]; 
+console.log(Componentes); 
