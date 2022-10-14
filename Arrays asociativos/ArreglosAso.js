a8252add6d7252cba7fa644e3263b8dc68189b2f
Matias Vargas <mat.dweb28[1>
2022-10-13 20:19:41 -0600

    desarrollo de contenido arrays asociativos

---
+++ Arrays asociativos/ArreglosAso.js
@@ -0,0 +1,30 @@
+//Hola - hoy aprenderemos que son los arreglos asociativos y como funcionan en JS 
+
+//Para empezar debemos entender que un arreglo asociativo
+
+//este es como un arreglo normal (un conjunto de elementos) solo que cada elemento que le conforma es otro arreglo con mas elementos o mejor llamdo objeto 
+
+//veamos un ejemplo de arreglo
+
+let array = [1, 2, 3]
+// este es un arreglo con elementos, cada uno representa solo un numero
+
+//pero que pasaria si cada representara otra lista de elementos tambien 
+
+let numeros  = { 
+    decenas: [10,20,30],
+    centanas: [100, 200, 300], 
+}
+
+//numeros esta confromado de dos elementos, pero cada uno esta conformada de tres valores
+
+//Apliquemos lo aprendido
+let componentes = {
+    Memoria: ["RAM","ROM", "Memoria central"], 
+    Procesador: ["UC", "UAL"],
+    HardwareExt: ["Teclado", "Mouse", "Memoria Ext"]
+}
+
+//Espera el siguiente video para apreder a manejar estos datos
+
+//Muchas gracias por ver :)
